a6eb69ecf345146f3a55b58065c5fd53bc2ad7de
minseok128 <minseok128128@gmail.com>
2024-02-08 21:58:36 +0900

    Update rasberry : 파일 받고 저장까지 성공!

--- routes/rasberry.js
+++ routes/rasberry.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const multer = require('multer');
-const path = require('path');
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+var path = require('path');
 
 // multer 설정: 오디오 파일을 디스크에 저장
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '../uploads/')  // 'uploads/'는 파일을 저장할 디렉토리
+        cb(null, 'uploads/')  // 'uploads/'는 파일을 저장할 디렉토리
     },
     filename: function (req, file, cb) {
         // 클라이언트에서 보낸 파일 이름을 사용
@@ -15,12 +15,6 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-
-// router.post('/', (req, res) => {
-//     console.log("Basic route test");
-//     res.send("Basic route working");
-// });
-
 
 router.post('/', upload.single('sound'), (req, res) => {
     console.log("here");
@@ -32,12 +26,12 @@ router.post('/', upload.single('sound'), (req, res) => {
         const latitude = req.body.latitude;
         const longtitude = req.body.longtitude;
 
-        // if (!req.file) {
-        //     throw new Error('File is not provided');
-        // }
+        if (!req.file) {
+            throw new Error('File is not provided');
+        }
 
         // 파일 저장 위치와 이름
-        // const filePath = path.join(__dirname, '../uploads', req.file.originalname);
+        const filePath = path.join(__dirname, 'uploads', req.file.originalname);
         
         res.send(`File saved successfully at ${filePath}`);
     } catch (err) {
